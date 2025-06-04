89cfbf375ee33c5a61957d674adc71a7de07f64d
Vincent Mcleese <mcleesevj@gmail.com>
2025-06-04 22:04:39 +0200

    Fix useAuth import in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useAuth } from '@/hooks/useAuth';
+import { useAuth } from '@/providers/AuthProvider';
 import { useState } from 'react';
 
 export default function Header() {
