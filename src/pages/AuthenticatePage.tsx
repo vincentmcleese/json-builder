7b7753683a7848ee79a9cdb853443939567f201a
Vincent Mcleese <mcleesevj@gmail.com>
2025-06-04 22:09:43 +0200

    Remove unused useNavigate import

--- src/pages/AuthenticatePage.tsx
+++ src/pages/AuthenticatePage.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '@/lib/supabase';
 import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@/providers/AuthProvider';
 
 interface AuthLog {
